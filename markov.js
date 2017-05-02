c825082d49a76747f3470eccd75dfb2cd6255ce7
EvilScott <reis.robert.s@gmail.com>
2017-05-01 18:09:41 -0700

    get markov prefix working

--- markov.js
+++ markov.js
@@ -3,37 +3,50 @@ const fs = require('fs'),
     Q = require('q'),
     redis = require('redis').createClient('6379', 'redis'),
     DICT_KEY = process.env.DICT_KEY || 'newsfaker:dict',
-    MAX_LENGTH = process.env.MAX_LENGTH || 10;
-    //TODO prefix setting
+    MAX_LENGTH = process.env.MAX_LENGTH || 30,
+    PREFIX = process.env.PREFIX || 3;
 
 const markov = {
     dictionaryExists: () => {
         return Q.ninvoke(redis, 'exists', DICT_KEY);
     },
 
+    removeDictionary: () => {
+        return Q.ninvoke(redis, 'del', DICT_KEY).then(() => {
+            console.log('Old dictionary removed...');
+        });
+    },
+
     generateDictionary: () => {
         return Q.nfcall(fs.readFile, 'headlines.txt', 'utf8').then(data => {
             let dict = {};
-            console.log('Generating markov dictionary...');
+            console.log('Generating dictionary...');
             data.split("\n").forEach(line => {
-                let words = `^ ${line.replace(/[^\w\s]/g, '')} $`.split(' ');
-                while (words.length > 1) {
-                    let word = words.shift();
-                    if (!dict.hasOwnProperty(word)) dict[word] = [];
-                    dict[word].push(words[0].trim());
+                let words = `^ ${line.replace(/[^\w\s]/g, '').toUpperCase()} $`.split(' ');
+                let prefixWords = words.splice(0, PREFIX);
+                while (words.length > PREFIX) {
+                    while (prefixWords.length < PREFIX) prefixWords.push(words.shift());
+                    if (!dict.hasOwnProperty(prefixWords.join(' '))) dict[prefixWords.join(' ')] = [];
+                    dict[prefixWords.join(' ')].push(words[0].trim());
+                    prefixWords.shift();
                 }
             });
+            console.log('Saving to redis...');
             const dictValues = _.flatten(_.map(dict, (val, key) => [key, val.join(',')]));
-            Q.ninvoke(redis, 'hmset', DICT_KEY, ...dictValues)
-                .then(_ => console.log('Finished generating dictionary!'))
+            return Q.ninvoke(redis, 'hmset', DICT_KEY, dictValues)
+                .then(_ => console.log('Finished!'))
                 .catch(err => console.log(err.stack));
         });
     },
 
-    generateHeadline: (headline = ['^']) => {
-        const lastWord = headline[headline.length - 1];
-        return Q.ninvoke(redis, 'hget', DICT_KEY, lastWord).then(words => {
-            if (lastWord !== null && lastWord !== '$' && headline.length < MAX_LENGTH) {
+    starterKey: () => {
+        return Q.ninvoke(redis, 'hkeys', DICT_KEY)
+            .then(keys => _.sample(keys.filter(key => key.split(' ')[0] === '^')));
+    },
+
+    generateHeadline: (headline) => {
+        return Q.ninvoke(redis, 'hget', DICT_KEY, headline.slice(-1 * PREFIX).join(' ')).then(words => {
+            if (words !== null && headline.slice(-1) !== '$' && headline.length < MAX_LENGTH) {
                 headline.push(_.sample(words.split(',')));
                 return markov.generateHeadline(headline);
             } else {
@@ -47,4 +60,4 @@ const markov = {
 
 module.exports = markov;
 
-if (require.main === module) markov.generateDictionary();
+if (require.main === module) markov.removeDictionary().then(markov.generateDictionary);
--- server.js
+++ server.js
@@ -10,18 +10,11 @@ app.get('/', (req, res) => res.render('index'));
 
 // markov endpoint
 app.get('/headline', (req, res) => {
-    markov.generateHeadline()
+    markov.starterKey()
+        .then(starter => markov.generateHeadline(starter.split(' ')))
         .then(val => res.json({ headline: val }))
         .catch(err => res.send(err.stack));
 });
 
-// generate dictionary if needed, then start server
-markov.dictionaryExists().then(exists => {
-    if (exists) {
-        app.listen(4000);
-    }  else {
-        markov.generateDictionary()
-            .then(_ => app.listen(4000))
-            .catch(err => console.log(err.stack));
-    }
-}).catch(err => console.log(err.stack));
+// start server
+app.listen(4000);
